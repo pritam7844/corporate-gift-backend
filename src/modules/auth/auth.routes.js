2b34a03655151c64944ed12aa38fecd33b45e84e
difmo <difmotech@gmail.com>
2026-03-08 16:29:55 +0530

    save

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -3,11 +3,7 @@ import { register, login, logout } from './auth.controller.js';
 import { isLoggedIn, isAdmin } from '../../middleware/auth.middleware.js';
 
 const router = express.Router();
-
-// Public Routes
 router.post('/login', login);
-
-
 router.post('/register', register);
 router.post('/logout', logout);
 
